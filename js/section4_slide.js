219ac82a0511090719c4ea20542143743c625d5d
gjr100g <93234774+LeeKeonJong@users.noreply.github.com>
2021-12-29 18:59:14 +0900

    업데이트

--- js/section4_slide.js
+++ js/section4_slide.js
@@ -1,9 +1,16 @@
 $(function(){
 
-$('.dot7').click(function(){
-	$('#section4_slide1:first').prependTO('#section4_hiddenbox');
-	$('#section4_slide2').stop().ani
+	$('.dot6').click(function(){
+		$('#section4_hiddenbox').stop().animate({marginLeft:0},1500);
+		$('.dot7').removeClass('active')
+		$('.dot6').addClass('active')
+	});	
 
-});
+	$('.dot7').click(function(){
+		$('#section4_hiddenbox').stop().animate({marginLeft:-1400},1500);
+		$('.dot7').addClass('active')
+		$('.dot6').removeClass('active')
+	});
 
+	
 });
