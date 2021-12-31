e0fe0fa2b1a1ac1498c0a87c76cedb2c7420c761
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2021-12-31 11:51:04 +0900

    업데이트

--- js/section5_mouseover.js
+++ js/section5_mouseover.js
@@ -1,7 +1,12 @@
 $(function(){
     $("#section5_textbox2").mouseover(function(){
-        $("#radius").hide(500);
-        $("#radius2").show(500);
+        $("#radius_box").stop().fadeOut(100);
+        $("#arrow").stop().show(100);
+        
+    });
+    $("#section5_textbox2").mouseleave(function(){
+        $("#arrow").stop().hide();
+        $("#radius_box").stop().fadeIn();
     });
 
 });
---
+++ js/section5_open.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $("#section5-1").click(function(){
+        window.open("https://youtu.be/Rt_UqUm38BI","","");
+    });
+    $("#section5-2").click(function(){
+        window.open("https://youtu.be/b1rk55G2reY","","");
+    });
+});
