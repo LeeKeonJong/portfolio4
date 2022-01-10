e3206fca9c5c93be64ccfc2c991f2337d4621a47
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2022-01-10 18:02:24 +0900

    업데이트

--- js/section4_slide.js
+++ js/section4_slide.js
@@ -1,8 +1,6 @@
 $(function(){
-
-	
-
-	$('.dot6').click(function(){
+	if (window.matchMedia("(min-width: 1400px)").matches) {
+		$('.dot6').click(function(){
 		$('#section4_hiddenbox').stop().animate({marginLeft:0},1500);
 		$('.dot7').removeClass('active')
 		$('.dot6').addClass('active')
@@ -13,6 +11,24 @@ $(function(){
 		$('.dot7').addClass('active')
 		$('.dot6').removeClass('active')
 	});
+	  } else {
+		$('.dot6').click(function(){
+			$('#section4_hiddenbox').stop().animate({marginLeft:0},6000);
+			$('.dot7').removeClass('active')
+			$('.dot6').addClass('active')
+		});	
+	
+		$('.dot7').click(function(){
+			$('#section4_hiddenbox').stop().animate({marginLeft:-1965},10000);
+			$('.dot7').addClass('active')
+			$('.dot6').removeClass('active')
+			
+		});
+		
+	  }
+	
+
+	
 
 	
 });
