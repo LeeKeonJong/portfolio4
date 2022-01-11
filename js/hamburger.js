8cdeb82abe3045d6ae9efd0561e2c6d80f2556da
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2022-01-11 18:24:09 +0900

    수정

--- js/hamburger.js
+++ js/hamburger.js
@@ -7,7 +7,7 @@ $("#ham").hover(function(){
 });
 
 $("#menu_ham").mouseleave(function(){
-    $("#menu_ham").css("right","-30%").css("transition","0.6s");
+    $("#menu_ham").css("right","-30%").css("transition","0.5s");
      
  });
 
