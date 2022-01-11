7781764df09e689404eed3064463eed8b976e839
LeeKeonJong <93234774+LeeKeonJong@users.noreply.github.com>
2022-01-11 17:47:08 +0900

    업데이트

---
+++ js/hamburger.js
@@ -0,0 +1,18 @@
+$(function(){
+
+
+$("#ham").hover(function(){
+   $("#menu_ham").css("right","0").css("transition","0.6s");
+    
+});
+
+$("#ham").mouseleave(function(){
+    $("#menu_ham").css("right","-30%").css("transition","0.6s");
+     
+ });
+
+
+
+
+
+});
